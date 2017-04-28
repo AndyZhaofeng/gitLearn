3f7e0a4925792d17f7643fb38aa6df76cdd8e5a8
赵峰 <zf526637166@gmail.com>
2017-04-28 08:59:42 +0800

    modified repo a bit

--- repo.rb
+++ repo.rb
@@ -1,1 +1,1 @@
-<html><body>You are being <a href="https://github.com/mojombo/grit/blob/master/lib/grit/repo.rb">redirected</a>.</body></html>
+<html><body>You are being <a href="https://github.com/mojombo/grit/blob/master/lib/grit/repo.rb">redirected</a>.</body></html># testing
